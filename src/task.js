f86499e783470f2e851e3e2021e38c4255ec3905
Francisco De La Cruz <helloquico@gmail.com>
2015-11-04 13:35:27 -0500

    Define Modules

---
+++ src/task.js
@@ -0,0 +1,11 @@
+define(function () {
+	'use strict';
+
+	function Task(name) {
+		this.completed = false;
+		this.name = name;
+	}
+	Task.prototype = {};
+	Task.prototype.constructor = Task;
+});
+
---
+++ src/ui_strings.js
@@ -0,0 +1,7 @@
+define({
+	welcome: 'Welcome to the TO-DO app!',
+	createText: 'Create list',
+	placeHolderText: 'List name',
+	noListsText: 'No saved lists',
+	loadText: 'Load list'
+});
