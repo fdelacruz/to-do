35ec10bded21b662426cd1487bafc13dff54cafb
Francisco De La Cruz <helloquico@gmail.com>
2015-11-08 22:51:19 -0500

    Use an Alternative Module Loader

--- build/build.js
+++ build/build.js
@@ -2,6 +2,6 @@
 	appDir: '../',
 	baseUrl: 'src',
 	dir: '../dist',
-	modules: [{ name: 'main' }],
-	mainConfigFile: '../src/config.js'
+	mainConfigFile: '../src/config.js',
+	name: '../external/almond'
 })
--- dist/build/build.js
+++ dist/build/build.js
@@ -1,1 +1,1 @@
-({appDir:"../",baseUrl:"src",dir:"../dist",modules:[{name:"main"}],mainConfigFile:"../src/config.js"});
+({appDir:"../",baseUrl:"src",dir:"../dist",mainConfigFile:"../src/config.js",name:"../external/almond"});
