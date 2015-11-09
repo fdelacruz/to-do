99a30afa1a60c4bf976ad99e4d93a32a56d24fc6
Francisco De La Cruz <helloquico@gmail.com>
2015-11-08 22:32:03 -0500

    Use the r.js Optimizer

---
+++ dist/src/nls/root/ui_strings.js
@@ -0,0 +1,1 @@
+define({welcome:"Welcome to the TO-DO app!",createText:"Create list",placeHolderText:"List name",noListsText:"No saved lists",loadText:"Load list",createTaskPlaceholder:"Task name",addText:"Add task",saveText:"Save list"});
